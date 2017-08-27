81367a82975ed2554c53e52c300e8ed0aaab5ac0
Dougy Ouyang <ouyangdougy@163.com>
2017-08-27 08:45:01 -0700

    Update 2.cpp

--- prelim/2.cpp
+++ prelim/2.cpp
@@ -6,16 +6,18 @@ using namespace std;
 int main()
 {
     string s;
+    long long po1, po2;
+    bool same=true;
     
     cin >> s;
-    long long po1=0, po2=s.length()-1;
-    while(po1<po2){
-        if(s[po1]!=s[po2])
-            break;
-        po1++, po2--;
-        if(po1>po2)
-            cout << "yes" << endl;
-    }
+    po1=0, po2=s.length()-1;
+    
+    while(po1<po2)
+        if(s[po1++]!=s[po2--])
+            same=false;
+    
+    if(same)
+            cout << "same" << endl;
     
     return 0;
 }

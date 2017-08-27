e919997ad19ca30d08f73d28d0c37bb68c2b7daa
Dougy Ouyang <ouyangdougy@163.com>
2017-08-27 01:08:02 -0700

    Create 3_faster.cpp

---
+++ prelim/3_faster.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, array, total, cur=0;
+    int i;
+    
+    cin >> n;
+    cin >> array;
+    
+    total=cur=array;
+    for(i=2;i<=n;i++){
+        cin >> array;
+        if(array>total && cur<0)
+            total=cur=array;
+        else
+            cur+=array;
+        total=max(total, cur);
+    }
+    
+    cout << total << endl;
+    
+    return 0;
+}

c7a6280706cb52ab8fdf4d5299e31b88a577466a
Dougy Ouyang <ouyangdougy@163.com>
2017-08-27 08:38:44 -0700

    Update 3.cpp

--- prelim/3.cpp
+++ prelim/3.cpp
@@ -5,14 +5,12 @@ int pre[10001];
 
 int main()
 {
-    int n, array[10001];
+    int n, array;
     int i, j, lar=0;
     
     cin >> n;
     for(i=1;i<=n;i++)
-        cin >> array[i];
-    for(i=1;i<=n;i++)
-        pre[i]=pre[i-1]+array[i];
+        cin >> array, pre[i]=pre[i-1]+array;
     
     for(i=0;i<n;i++)
         for(j=1;j<=n;j++)
